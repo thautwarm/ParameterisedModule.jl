fe8c947cb06d36af98a1cc05d25e0f7f388461cb
thautwarm <twshere@outlook.com>
2019-11-11 16:45:36 +0800

    done

--- README.md
+++ README.md
@@ -4,3 +4,101 @@
 [![Dev](https://img.shields.io/badge/docs-dev-blue.svg)](https://thautwarm.github.io/ParameterisedModule.jl/dev)
 [![Build Status](https://travis-ci.com/thautwarm/ParameterisedModule.jl.svg?branch=master)](https://travis-ci.com/thautwarm/ParameterisedModule.jl)
 [![Codecov](https://codecov.io/gh/thautwarm/ParameterisedModule.jl/branch/master/graph/badge.svg)](https://codecov.io/gh/thautwarm/ParameterisedModule.jl)
+
+
+ML parameterised modules in Julia.
+
+# APIs
+
+- `@sig struct ... end` : define module signatures, like `sig` in OCaml.
+- `@structure struct ... end` : define module structures, like `struct` in OCaml.
+- `@open ModuleType Module` : using module, like `open` in OCaml.
+- `@open ModuleType Module body` : using module when evaluating `body`, like `let open` in OCaml.
+
+# Non-Parametric Example
+
+```julia
+using ParameterisedModule
+
+# this is the module type declaration
+@sig struct NatAlgebra
+    struct Eltype end # this is type declaration
+    succ :: Function
+    zero :: Eltype
+end
+
+# make a module `num_nat`, whose module type is NatAlgebra
+num_nat = @structure struct NatAlgebra
+    Eltype  = Int
+    succ(x) = x + 1
+    zero    = 0
+end
+
+@open NatAlgebra num_nat begin
+    println(succ(succ(zero))) # 2
+end
+
+println(succ(succ(zero)))
+# ERROR: UndefVarError: succ not defined
+
+str_nat = @structure struct NatAlgebra
+    Eltype  = String
+    succ(x) = "succ($x)"
+    zero    = "zero"
+end
+
+@open NatAlgebra str_nat begin
+    println(succ(succ(zero))) # succ(succ(zero))
+end
+```
+
+# Parametric Examples
+
+```julia
+
+Functor = Function
+@sig struct TF{Eltype}
+    e :: Eltype
+end
+
+TFZero(nat :: NatAlgebra) =
+    @structure struct TF{nat.Eltype}
+        e = nat.zero
+    end
+
+word_algebra =
+    @structure struct NatAlgebra
+        Eltype = Functor
+        zero = TFZero
+        succ(T1) =
+            function (N::NatAlgebra)
+                @structure struct TF{N.Eltype}
+                    e = N.succ(T1(N).e)
+                end
+            end
+    end
+
+@sig struct H
+    h :: Function
+end
+
+HTFC(N::NatAlgebra) =
+    @structure struct H
+        h(T) = T(N).e
+    end
+
+using Test
+@open H HTFC(num_nat) begin
+    @test h(word_algebra.zero) == num_nat.zero
+
+    case(x::Functor) =
+        h(word_algebra.succ(x)) == num_nat.succ(h(x))
+
+    words = Functor[TFZero]
+    for i = 1:100
+        push!(words, word_algebra.succ(words[end]))
+    end
+    @test all(words) do x; case(x) end
+end
+
+```
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,151 @@
 using ParameterisedModule
 using Test
 
-@testset "ParameterisedModule.jl" begin
+Functor = Function
+
     # Write your own tests here.
+
+    # :(@sig struct S{A}
+#     x :: Int
+#     y :: A
+#     struct K end
+# end) |> (x -> macroexpand(ParameterisedModule, x)) |> println
+
+
+@testset "I'm here?" begin
+
+@sig struct S{A}
+    x :: Int
+    y :: A
+    struct K end
+end
+
+println(S)
+
+# :(@structure struct S{String}
+#     struct K
+#         x :: Int
+#     end
+#     x = 2
+#     y = "2"
+# end) |> (x -> macroexpand(ParameterisedModule, x)) |> println
+
+mod1 = @structure struct S{String}
+    struct K
+        x :: Int
+    end
+    x = 2
+    y = "2"
+end
+
+@test mod1.x == 2
+@test mod1.y == "2"
+@test_throws Any mod1.x = 3
+
+
+@test_throws Any begin
+    @structure struct S{Nothing}
+    struct K_ end
+        K = K_
+        x = 0
+        y = "2" # should be nothing
+    end
+end
+
+
+@sig struct Numeric
+    (+) :: Function
+    (-) :: Function
+end
+
+mod3 = @structure struct Numeric
+    a + b = "$a + $b"
+    a - b = "$a - $b"
+end
+
+
+@test mod3.:+(1, 2) == "1 + 2"
+@test mod3.:-(1, 2) == "1 - 2"
+
+@open Numeric mod3 begin
+    @test 1 + 2 == "1 + 2"
+end
+
+
+using ParameterisedModule
+
+# this is the module type declaration
+@sig struct NatAlgebra
+    struct Eltype end # this is type declaration
+    succ :: Function
+    zero :: Eltype
+end
+
+# make a module `num_nat`, whose module type is NatAlgebra
+num_nat = @structure struct NatAlgebra
+    Eltype  = Int
+    succ(x) = x + 1
+    zero    = 0
+end
+
+@open NatAlgebra num_nat begin
+    println(succ(succ(zero))) # 2
+end
+
+@test_throws UndefVarError println(succ(succ(zero)))
+# ERROR: UndefVarError: succ not defined
+
+str_nat = @structure struct NatAlgebra
+    Eltype  = String
+    succ(x) = "succ($x)"
+    zero    = "zero"
+end
+
+@open NatAlgebra str_nat begin
+    println(succ(succ(zero))) # succ(succ(zero))
+end
+
+@sig struct TF{Eltype}
+    e :: Eltype
+end
+
+TFZero(nat :: NatAlgebra) =
+    @structure struct TF{nat.Eltype}
+        e = nat.zero
+    end
+
+word_algebra =
+    @structure struct NatAlgebra
+        Eltype = Functor
+        zero = TFZero
+        succ(T1) =
+            function (N::NatAlgebra)
+                @structure struct TF{N.Eltype}
+                    e = N.succ(T1(N).e)
+                end
+            end
+    end
+
+@sig struct H
+    h :: Functor
+end
+
+word_algebra.succ(TFZero)(num_nat) |> println
+
+HTFC(N::NatAlgebra) =
+    @structure struct H
+        h(T) = T(N).e
+    end
+
+@open H HTFC(num_nat) begin
+    @test h(word_algebra.zero) == num_nat.zero
+    case(x::Functor) =
+        h(word_algebra.succ(x)) == num_nat.succ(h(x))
+
+    words = Functor[TFZero]
+    for i = 1:100
+        push!(words, word_algebra.succ(words[end]))
+    end
+    @test all(words) do x; case(x) end
+end
 end
